ba0b53e0b9e4bd0757b394a161c35ac77b34f0ed
James-Livesey <jamesliveg@gmail.com>
2019-09-02 17:01:19 +0100

    Add trail details

--- script.js
+++ script.js
@@ -60,13 +60,31 @@ $(function() {
             var trailText = "";
 
             if (trailNumber == "1") {
-                trailText = "Welcome to the NOSO™CON™ official trail! To continue, go to trail 2.";
+                trailText = "Welcome to the official NOSO™CON™ trail! To continue, you’ll need to find the secret lair where the NOSO™ developers hide!";
+            } else if (trailNumber == "2") {
+                trailText = "Here’s where the NOSO™ developers hide! It is usually a top-secret zone where the organisers of NOSO™CON™ plan their events. In here, plenty of coding gets done, including the coding made precisely for this trail!<br><br>Speaking of this trail, let’s go to number 3: the place where all of the NOSO™ food gets cooked up every day! You’ll need to find the code behind the hob…";
+            } else if (trailNumber == "3") {
+                trailText = "So here’s the place where lots of tasty NOSO™ food gets cooked up every day! The food is delicious, and some of the drinks have been made for you to enjoy here at NOSO™CON™!<br><br>Now we’re going to be going to the place where the dishes get scrubbed and washed, and where NTombi has her food and drink!";
+            } else if (trailNumber == "4") {
+                trailText = "So here’s where the dishes get scrubbed and washed after a good meal! You’ll need to go outside for the next one, so put your shoes on and head out through the door!<br><br>Keep your eyes peeled for the big tree with loads of hoses on it! There’s a code hidden there.";
+            } else if (trailNumber == "5") {
+                trailText = "So this tree is very old! The tree has got hoses attached to it so you can climb it. Don’t worry; you won’t need to climb the tree today!<br><br>Now time to go to the small building where Forg sleeps! The building should be found at the other side of the green grass.";
+            } else if (trailNumber == "6") {
+                trailText = "Here’s the place where the almighty Forg takes a nap! He enjoys the comfort of this building, as it is small enough and comfy.<br><br>It’s not just Forg who likes to take a nap every now and then, Ben and Issy like to take a nap too! You’ll need to cross the grass and find the dangly tree where there is a small garden shed fit for two adventurers!";
+            } else if (trailNumber == "7") {
+                trailText = "This is where the adventurers Ben and Issy like to sleep! Every night they like to tuck into this little abode. In the morning, they like to sneak over to the main house and find the NOSO™s!<br><br>To find the NOSO™s yourself, you’ll need to go to the main house (enter by the door on the left by the tree). Then, go upstairs to the landing!";
+            } else if (trailNumber == "8") {
+                trailText = "This landing is famous for its popularity of NOSO™s! There are many NOSO™s, but there’s one we really need to find: Neeka, the main NOSO™!<br><br>Find the bedroom in the middle to the right to the desk, and open the door. That’s where Neeka awaits!";
+            } else if (trailNumber == "9") {
+                trailText = "Is Neeka sitting next to the code you scanned? If so, you win the trail! Take Neeka with you and bring him downstairs to the NOSO™CON™ event room.<br><br>If he isn’t sitting next to the code, sorry but someone took him first! Don’t worry, there are drinks in the NOSO™CON™ event room for you to enjoy!";
+            } else if (trailNumber == "10") {
+                trailText = "Thanks for playing the NOSO™CON™ official trail! We hope you enjoyed the trail very much! Come again soon for NOSO™CON™ 2020 or even another one this year!";
             } else {
                 trailText = "Hmm, this trail number seems incorrect. Please try another trail number!";
             }
 
             $(".trailNumber").text(trailNumber);
-            $(".trailText").text(trailText);
+            $(".trailText").html(trailText);
 
             $(".scanTrail").show();
             } catch {
